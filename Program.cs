b759f31a6810acae2fffc9921a2c5cf762be4ed5
Thomas Kaufmann <thomaskaufmann@Thomass-MBP.home>
2018-02-18 17:34:03 +0100

    add files

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SimpleSteemRPC
+{
+    class Program
+    {
+        private static SimpleSteemRPC ssr;
+        
+        static void Main(string[] args)
+        {
+            ssr = new SimpleSteemRPC();
+            Console.WriteLine("Anzahl der Konten/Number of accounts:");
+            Console.WriteLine(ssr.FormatJSON(ssr.Transaction("get_account_count")));
+        }
+    }
+}
